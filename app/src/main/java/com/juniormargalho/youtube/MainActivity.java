8dcfdb53f0c4baaadd50d43e40d15a224d83bfa6
juniormargalho <juniormargalho@gmail.com>
2020-04-29 23:37:38 -0300

    configuradoAPIYoutube

--- app/src/main/java/com/juniormargalho/youtube/MainActivity.java
+++ app/src/main/java/com/juniormargalho/youtube/MainActivity.java
@@ -6,9 +6,12 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String GOOGLE_API_KEY = "AIzaSyAIA4u-pSxVeSxVOO8P_-584lqn05-FEnQ";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
