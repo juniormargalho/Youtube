1b410f70223057e74a7cf4b748e0ac07cd228909
juniormargalho <juniormargalho@gmail.com>
2020-04-30 23:53:32 -0300

    configuradoPlayerTesteVideo

--- app/src/main/java/com/juniormargalho/youtube/MainActivity.java
+++ app/src/main/java/com/juniormargalho/youtube/MainActivity.java
@@ -3,15 +3,35 @@ package com.juniormargalho.youtube;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 
+public class MainActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
     private static final String GOOGLE_API_KEY = "AIzaSyAIA4u-pSxVeSxVOO8P_-584lqn05-FEnQ";
+    private YouTubePlayerView youTubePlayerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        youTubePlayerView = findViewById(R.id.viewYoutubePlayer);
+        youTubePlayerView.initialize(GOOGLE_API_KEY, this);
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean foiRestaurado) {
+        if(!foiRestaurado){
+            youTubePlayer.cueVideo("C4nursAx-hY");
+        }
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+        Toast.makeText(this, "Erro ao iniciar o player!", Toast.LENGTH_SHORT).show();
     }
 }
