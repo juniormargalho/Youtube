54a164d56a726c0c532d40b4c5c2e84d3029be61
juniormargalho <juniormargalho@gmail.com>
2020-05-06 11:31:31 -0300

    configChanelID&URLBase

--- app/src/main/java/com/juniormargalho/youtube/helper/YoutubeConfig.java
+++ app/src/main/java/com/juniormargalho/youtube/helper/YoutubeConfig.java
@@ -1,8 +1,8 @@
 package com.juniormargalho.youtube.helper;
 
 public class YoutubeConfig {
-    public static String CANAL_ID = "";
+    public static String CANAL_ID = "UCrOH1V-FyMunBIMrKL0y0xQ";
     public static String CHAVE_YOUTUBE_API = "AIzaSyA2J20dDrvBOTJSgeby76HkTz7SzBhL2WI";
-    public static String URL_BASE = "";
+    public static String URL_BASE = "https://www.googleapis.com/youtube/v3/";
 
 }
